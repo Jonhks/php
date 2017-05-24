042f15e2f9fb7d13f7863d1749bd95d4742e0545
jonhlab <jonathan@laboratoria.mx>
2017-05-24 09:45:49 -0500

    prueba 2

--- archivos.php
+++ archivos.php
@@ -1,22 +1,28 @@
-<?php 
+<?php 	
 
-// if ( file_exists('documento.txt') ) {
-// 	echo "El archivo si existe";
-// } else {
-// 	echo "El archivo no existe";
+// if (file_exists('documento.txt')) {
+// 	echo "El archivo existe";
+// }else {
+// 	echo "El archivo No existe";
 // }
 
+
+								// Leer la info desde un documento
 // echo file_get_contents('documento.txt');
 
-// file_put_contents('documento.txt', "Hola Mundo \n", FILE_APPEND);
+									
+									// Escribir en un documento
+// file_put_contents('documento.txt', "Hola Jonh \n", FILE_APPEND);
 
+									// agregar numeros
 // file_put_contents('documento.txt', '');
-// for ($i=1; $i <=10 ; $i++) { 
-// 	file_put_contents('documento.txt', "$i \n", FILE_APPEND);
-// }
+// 	for ($i=1; $i <=10 ; $i++) { 
+// 		file_put_contents('documento.txt', "$i \n", FILE_APPEND);
+		
+// 	}
 
 $archivo = file('documento.txt');
-echo "<pre>";
+echo  "<pre>";
 print_r($archivo);
 
 ?>
---
+++ funciones_rutas.php
@@ -0,0 +1,23 @@
+<?php 
+
+						// Extencion del archivo
+// echo pathinfo('documento.txt', PATHINFO_BASENAME);
+
+							// El direcorio
+// echo pathinfo('documento.txt', PATHINFO_DIRNAME);
+
+							// Muestra el tipo de archivo
+// echo pathinfo('documento.txt', PATHINFO_EXTENSION);
+
+							//Extraer el nombre del archivo
+// echo pathinfo('documento.txt', PATHINFO_FILENAME);
+
+							//Traer todos los archivos con una terminacion('a*.php');(para ver con iniciales)('*{php,txt}', GLOB_BRACE);*(Para ver muchas extenciones)
+//$resultados = glob('*.php');
+//print_r($resultados);
+
+//basename(caprpeta1/carpeta2/archivo.php)
+							//el directorio padre de la ruta
+//dirname(caprpeta1/carpeta2/archivo.php)
+
+ ?>
